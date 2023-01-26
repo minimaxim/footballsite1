1e93f3f1ebda61d91c3c93915781f42a9f4ee2a7
Maxim <maxxim.zhurkin99@gmail.com>
2023-01-26 19:18:50 +0300

    delete error 404

--- football/settings.py
+++ football/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n@#uk$bslijaxnk8=k1)g-i99o+fhm+5flvp)fm*$fz0eir=hm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
